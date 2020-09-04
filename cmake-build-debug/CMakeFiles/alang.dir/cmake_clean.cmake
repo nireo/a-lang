1f0f7eb8cd5bb39d19dc7eba1e917d1589d62775
nireo <eemil.lehtikangas1@gmail.com>
2020-09-04 21:10:16 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/alang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alang.dir/main.cpp.o"
+  "alang"
+  "alang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/alang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
